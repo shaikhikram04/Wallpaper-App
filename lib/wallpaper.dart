b4a8412df2fb078b2593c8b7c93f64798342ba5b
Ikram Shaikh <ikramshaikh0480@gmail.com>
2024-10-29 21:47:42 +0530

    wrote the function for fetching the data

--- lib/wallpaper.dart
+++ lib/wallpaper.dart
@@ -1,7 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:http/http.dart' as http;
 
 class Wallpaper extends StatefulWidget {
   const Wallpaper({super.key});
+
+  fetchApi() async {
+    await http.get(Uri.parse('https://api.pexels.com/v1/curated?per_page=80'),
+        headers: {
+          'Authorization': dotenv.env['PEXEL_API_KEY']!,
+        }).then(
+      (value) {
+        print('------------------');
+        print(value);
+        print('------------------');
+      },
+    );
+  }
 
   @override
   State<Wallpaper> createState() => _WallpaperState();
@@ -9,20 +24,42 @@ class Wallpaper extends StatefulWidget {
 
 class _WallpaperState extends State<Wallpaper> {
   @override
+  void initState() {
+    super.initState();
+    
+  }
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: [
           Expanded(
-            child: Container(),
+            child: GridView.builder(
+              itemCount: 80,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+                crossAxisSpacing: 2,
+                childAspectRatio: 2 / 3,
+                mainAxisSpacing: 2,
+              ),
+              itemBuilder: (context, index) {
+                return Container(
+                  color: Colors.white,
+                );
+              },
+            ),
           ),
           Container(
+            height: 60,
+            width: double.infinity,
             color: Colors.black,
-            child: const Text(
-              'Load more',
-              style: TextStyle(
-                fontSize: 20,
-                color: Colors.white,
+            child: const Center(
+              child: Text(
+                'Load more',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                ),
               ),
             ),
           ),
