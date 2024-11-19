d89a38e7fff3d1e4b915b4398428ec369562a265
Ikram Shaikh <ikramshaikh0480@gmail.com>
2024-11-19 21:18:40 +0530

    added preview only for web

--- lib/main.dart
+++ lib/main.dart
@@ -16,38 +16,40 @@ Future<void> main() async {
   }
 
   runApp(
-    DevicePreview(
-      backgroundColor: Colors.white,
-      enabled: true,
-      defaultDevice: Devices.ios.iPhone13ProMax,
-      isToolbarVisible: true,
-      availableLocales: const [Locale('en', 'US')],
-      tools: const [
-        DeviceSection(
-          model: true,
-          orientation: false,
-          frameVisibility: false,
-          virtualKeyboard: false,
-        ),
-      ],
-      devices: [
-        Devices.android.samsungGalaxyA50,
-        Devices.android.samsungGalaxyNote20,
-        Devices.android.samsungGalaxyS20,
-        Devices.android.samsungGalaxyNote20Ultra,
-        Devices.android.onePlus8Pro,
-        Devices.android.sonyXperia1II,
-        Devices.ios.iPhoneSE,
-        Devices.ios.iPhone12,
-        Devices.ios.iPhone12Mini,
-        Devices.ios.iPhone12ProMax,
-        Devices.ios.iPhone13,
-        Devices.ios.iPhone13ProMax,
-        Devices.ios.iPhone13Mini,
-        Devices.ios.iPhoneSE,
-      ],
-      builder: (context) => const MyApp(),
-    ),
+    kIsWeb
+        ? DevicePreview(
+            backgroundColor: Colors.white,
+            enabled: true,
+            defaultDevice: Devices.ios.iPhone13ProMax,
+            isToolbarVisible: true,
+            availableLocales: const [Locale('en', 'US')],
+            tools: const [
+              DeviceSection(
+                model: true,
+                orientation: false,
+                frameVisibility: false,
+                virtualKeyboard: false,
+              ),
+            ],
+            devices: [
+              Devices.android.samsungGalaxyA50,
+              Devices.android.samsungGalaxyNote20,
+              Devices.android.samsungGalaxyS20,
+              Devices.android.samsungGalaxyNote20Ultra,
+              Devices.android.onePlus8Pro,
+              Devices.android.sonyXperia1II,
+              Devices.ios.iPhoneSE,
+              Devices.ios.iPhone12,
+              Devices.ios.iPhone12Mini,
+              Devices.ios.iPhone12ProMax,
+              Devices.ios.iPhone13,
+              Devices.ios.iPhone13ProMax,
+              Devices.ios.iPhone13Mini,
+              Devices.ios.iPhoneSE,
+            ],
+            builder: (context) => const MyApp(),
+          )
+        : const MyApp(),
   );
 }
 
