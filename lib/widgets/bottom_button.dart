b7de940c4239d98ae919765fd5c7bae71d317d17
Ikram Shaikh <ikramshaikh0480@gmail.com>
2024-11-19 16:31:38 +0530

    showing linear loading indicator on loadmore images

---
+++ lib/widgets/bottom_button.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class BottomButton extends StatelessWidget {
+  const BottomButton({super.key, required this.onTap, required this.text});
+
+  final void Function() onTap;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        height: 60,
+        width: double.infinity,
+        color: Colors.black,
+        child: Center(
+          child: Text(
+            text,
+            style: const TextStyle(
+              fontSize: 20,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
